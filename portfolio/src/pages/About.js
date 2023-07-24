394038f3909d744949585cf0c9e37e3e3441bf79
Erik Martín Garzón <erikmarting1200@gmail.com>
2023-07-24 16:33:16 +0200

    Icons

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -8,12 +8,16 @@ export default function About() {
         <div className='about'>
             <div className='about-text'>
                 <h1>ABOUT ME</h1>
-                <p>I'm a pasionate videogame designer and I also have some game programming and artist skills.</p>
-                <p>during my studies I have participated in different projects that have helped me to increase my skills</p>
-                <p>in the different areas that are part of the world of videogames, as well as communication with different teams.</p>
-                <br></br>
-                <p>I have an unbreakable passion for design in general, and I transform every case and work ino an opportunity to</p>
-                <p>learn new things and acquire knowledge. I love programming and art too, and I have no difficulties with adapting to other positions and communicating with all the diferent fields of this sector. Furthermore, I am a very perfectionist worker, which puts quality at the top of priority list.</p>
+                <p>
+                    I'm a pasionate videogame designer and I also have some game programming and artist skills.
+                    during my studies I have participated in different projects that have helped me to increase my skills
+                    in the different areas that are part of the world of videogames, as well as communication with different teams.
+                </p>
+                <br/>
+                <p>
+                    I have an unbreakable passion for design in general, and I transform every case and work ino an opportunity to
+                    learn new things and acquire knowledge. I love programming and art too, and I have no difficulties with adapting to other positions and communicating with all the diferent fields of this sector. Furthermore, I am a very perfectionist worker, which puts quality at the top of priority list.
+                </p>
 
             </div>
            
